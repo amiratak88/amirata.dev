b048c56f9f3db8665b7b13424c9960a5c68723a1
Amirata Khodaparast <amiratak88@gmail.com>
2022-05-09 00:37:56 -0400

    "Recent Blogs" -> "Recent Posts"

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -19,9 +19,9 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 
 const Blog: NextPage<Props> = ({ postPreviews }) => {
 	return (
-		<Page title="Recent Blogs - Amirata">
+		<Page title="Recent Posts - Amirata">
 			<ReadingContainer>
-				<h1 className="mb-12 text-3xl font-bold">Recent Blogs</h1>
+				<h1 className="mb-12 text-3xl font-bold">Recent Posts</h1>
 				<div className="space-y-8">
 					{postPreviews.map((preview) => (
 						<BlogPostPreview key={preview.slug} {...preview} />
